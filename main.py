c2dd90d23f8dc2ff15953a582a20ce0e4b270d03
tbatko <tbatko@tafirma.com>
2024-11-09 18:37:23 +0100

    player turning

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0,0,0))
+        
+        screen.fill("black")
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
+
+        # limit framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
